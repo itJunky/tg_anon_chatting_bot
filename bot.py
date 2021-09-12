e40996cd64b3f1e7d67a8e5580e1f90924203167
Max <alphaQu4z4r@gmail.com>
2021-09-12 13:57:38 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import telebot
+from config import token
+
+@bot.message_handler(func=lambda message: message.forward_from_chat, content_types=["text", "photo", "video"])
+def posts_from_channels(msg):
+    # Remove messages from blocked groups
+    print(msg.chat.title + ' forward')
+    if msg.forward_from_chat.id in blocked_groups:
+        delete_messge(msg)
+
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
+def check_join_messages(msg):
+    bot.send_message(msg.chat.id, 'New user joined')
