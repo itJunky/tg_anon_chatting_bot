746bc9b8e84022d362d375748a3b22f973327f7c
Max <alphaQu4z4r@gmail.com>
2021-09-12 16:27:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,12 @@ def posts_from_channels(msg):
     print(msg.chat.title + ' forward')
     if msg.forward_from_chat.id in blocked_groups:
         delete_messge(msg)
-
-
+        
 @bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
 def check_join_messages(msg):
     bot.send_message(msg.chat.id, 'New user joined')
+    bot.promote_chat_member(msg.chat.id, msg.from_user.id, is_anonymous=True)
+
 
 if __name__ == '__main__':
     print("Censoro_bot Started")
